c85ed3f41036476de862fb01bdb2184308d6554c
ArexDiniz <alexgdiniz18@gmail.com>
2025-02-22 11:23:49 -0300

    Finalizando projeto

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ console.log('Hello, world!')
 
 const item = document.getElementById('item')
 const buttonAdd = document.getElementById('button-add')
+const container = document.querySelector('.container')
+
 const listProducts = document.getElementById('listProducts')
+
 
 
 buttonAdd.addEventListener('click', () => {
@@ -34,5 +37,37 @@ function addListItem(produto){
 
        listProducts.innerHTML += divContent
 }
+
+listProducts.onclick = (event) => {
+    if(event.target.className === 'trash'){
+        event.target.parentElement.remove()
+        removeAlert()
+
+    } 
+}
+
+function removeAlert(){
+    const divContent = `
+       <div class="remove-alert">
+            <img src="assets/alert.svg" alt="">
+            <span>O item foi removido da lista</span>
+            <button class="close-alert"><img src="assets/delete.svg" alt=""></button>
+        </div>`
+
+
+        container.insertAdjacentHTML("beforeend", divContent);
+        
+        const closeAlert = document.querySelector('.close-alert')
+        closeAlert.onclick = () => {
+            document.querySelector('.remove-alert').remove()
+        }
+        setTimeout(() => {
+            document.querySelector('.remove-alert').remove()
+        }, 2000)
+
+
+}
+
+
 
 
